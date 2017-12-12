f734c50cbc8839be8f25fc1ad09322bb3ca6b808
Lorna Jane Mitchell <lorna@lornajane.net>
2017-12-12 11:14:19 +0000

    Add better message formatting and error handling on incoming webhook

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ if (!process.env.token) {
 
 var Botkit = require('botkit');
 var os = require('os');
+var he = require('he');
+var striptags = require('striptags');
 
 var controller = Botkit.slackbot({
 // debug: true,
@@ -45,47 +47,73 @@ controller.on('reaction_added', function(bot, message) {
     if(message.item.channel && message.item.channel == "C4AB39ABH") {  // TODO configurable channel name please
         bot.api.users.info({user: message.user}, function(err, response) {
             user = response.user;
-            var msg = {
-                type: "message",
-                channel: "C4AB39ABH", // TODO this should be configurable
-                text: user.name + " reacted with " + message.reaction
-            }
+            var reaction = user.name + " reacted with " + message.reaction;
             bot.api.conversations.history({channel: message.item.channel, latest: message.item.ts, inclusive: true, limit:1}, function(err, response) {
                 var orig_message = response.messages[0].text;
-                console.log(orig_message);
-                msg.text += " to this message: " + orig_message;
-                bot.say(msg);
+                reaction += " to this message: " + orig_message;
+                console.log(reaction);
             });
         });
     }
 });
-
-
 
 /** Support incoming web requests **/
 controller.setupWebserver(3000, function(err, express_webserver) {
     express_webserver.get('/', function(req, res) {
         res.send('I am a bot');
 
-        var msg1 = {title: "Going fancy message styleee", color: "#0099cc", author: "Lorna", text: "This is the real message"};
         var msg = {
             type: "message",
             channel: "C4AB39ABH", // TODO this should be configurable
-            text: "You pinged me!  Ouch!",
-            attachments: [ msg1 ]
+            text: "Someone rang? (I received a web request)"
         };
         bot.say(msg);
     });
 
     express_webserver.post('/stackoverflow/incoming', function(req, res) {
         console.log(req.body);
-        var msg1 = {title: "Message received, loud and clear", text: req.body.message};
-        var msg = {
-            type: "message",
-            channel: "C4AB39ABH", // TODO this should be configurable
-            attachments: [ msg1 ]
-        };
-        bot.say(msg);
-        res.send('OK');
+        // only handling the event type "new-question" so far
+        var q;
+
+        if(typeof req.body.data !== 'undefined' 
+            && typeof req.body.data.question !== 'undefined') {
+            q = req.body.data.question;
+
+            var text = "<" + q.link + "| view on stackoverflow> or <https://sodashboard.mybluemix.net/home.html#edit?" + q.question_id + "| view on the dashboard>";
+            if(q.body) {
+                text += "\n" + striptags(q.body.substring(0,300));
+                if(q.body.length > 300) {
+                    text += " ...";
+                }
+            }
+
+            var msg1 = {
+                title: he.decode(q.title),
+                color: "#e09900",
+                text: text,
+                fields:
+                    [
+                    {
+                        title: "Author",
+                        value: q.owner.display_name,
+                        short: true
+                    },
+                    {   title:"Tagged",
+                        value: q.tags.join(", "),
+                        short:true
+                    }
+                    ]
+            };
+            var msg = {
+                type: "message",
+                channel: "C4AB39ABH", // TODO this should be configurable
+                attachments: [ msg1 ]
+            };
+            bot.say(msg);
+            res.send('OK');
+        } else {
+            res.status(400);
+            res.send('This endpoint expects a specific data structure; see docs');
+        }
     });
 });
